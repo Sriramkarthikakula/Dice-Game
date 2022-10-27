fe131a076d620bb1d3c876166735e4201bc14af1
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2022-10-27 20:56:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-alert("shall we begin the game");
-var random1 = Math.floor(Math.random()*6)+1;
-var imageselector1 = "dice"+random1+".png";
-document.querySelector(".img1").setAttribute("src",imageselector1);
+function cliked(){
+    var random1 = Math.floor(Math.random()*6)+1;
+    var imageselector1 = "dice"+random1+".png";
+    document.querySelector(".img1").setAttribute("src",imageselector1);
 
 
-var random2 = Math.floor(Math.random()*6)+1;
-var imageselector2 = "dice"+random2+".png";
-document.querySelector(".img2").setAttribute("src",imageselector2);
+    var random2 = Math.floor(Math.random()*6)+1;
+    var imageselector2 = "dice"+random2+".png";
+    document.querySelector(".img2").setAttribute("src",imageselector2);
 
-if(random1>random2)
-{
-    document.querySelector("h1").innerHTML="🚩Player 1 Wins";
-}
-else if(random1<random2)
-{
-    document.querySelector("h1").innerHTML="Player 2 Wins🚩";
-}
-else{
-    document.querySelector("h1").innerHTML="Draw!";
+    if(random1>random2)
+    {
+        document.querySelector("h1").innerHTML="🚩Player 1 Wins";
+        var audio = new Audio("claps.mp3");
+        audio.play();
+    }
+    else if(random1<random2)
+    {
+        document.querySelector("h1").innerHTML="Player 2 Wins🚩";
+        var audio = new Audio("claps.mp3");
+        audio.play();
+    }
+    else{
+        document.querySelector("h1").innerHTML="Draw!";
+    }
 }
